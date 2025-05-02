fa5a70cfdb4d876d854de300ee34e69a05755d1a
Acr_vini <Vacr750@gmail.com>
2025-05-02 14:58:55 -0300

    fixing dark and light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +19,16 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     RouterLink,
     RouterOutlet,
     CommonModule,
-    MatToolbarModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatSlideToggleModule,
+    MatToolbarModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
