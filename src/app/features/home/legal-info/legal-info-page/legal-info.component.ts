4c94146ad71683625304c57233c2602960bfe90d
Acr_vini <Vacr750@gmail.com>
2025-06-18 08:15:23 -0300

    feat: icon in legal info and activity-date

--- src/app/features/home/legal-info/legal-info-page/legal-info.component.ts
+++ src/app/features/home/legal-info/legal-info-page/legal-info.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-legal-info',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './legal-info.component.html',
   styleUrl: './legal-info.component.scss',
 })
